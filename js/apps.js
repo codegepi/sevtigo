b90f6bf1dad5fbca9f8e831cec516dbfeb459393
Sevtigo Pradono Wirawan <save.tigo@gmail.com>
2015-07-03 09:45:56 +0700

    add number coloumn to the result table

--- js/apps.js
+++ js/apps.js
@@ -1,21 +1,22 @@
 $(document).ready(function(){
-
+	var number = 1;
 	$("#submitContact").click(function(){
 		firstName = $("#firstName").val();
 		lastName = $("#lastName").val();
 		address = $("#address").val();
 		birthDate = $("#birthDate").val();
-		contactData =""
+		contactData ="";
 
 		if (firstName===""||lastName===""||address===""||birthDate==="") {
 			alert("Please fill all of your data");
 		} else {
-			$("tbody").append("<tr><td>"+firstName+"</td><td>"+lastName+"</td><td>"+address+"</td><td>"+birthDate+"</td></tr>");
+			$("tbody").append("<tr><td>"+number+"</td><td>"+firstName+"</td><td>"+lastName+"</td><td>"+address+"</td><td>"+birthDate+"</td></tr>");
 			$("#firstName").val("");
 			$("#lastName").val("");
 			$("#address").val("");
 			$("#birthDate").val("");
 			$("#firstName").focus();
+			number += 1;
 		};
 
 		return false;
